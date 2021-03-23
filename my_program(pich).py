d0b319211e13ccc6a9161a5b23232c085596d37a
AmirMahdyTurani <81232088+AmirMahdyTurani@users.noreply.github.com>
2021-03-23 15:34:14 +0430

    Update Main file & add coment to this file

--- my_program(pich).py
+++ my_program(pich).py
@@ -1,11 +1,19 @@
+#بسم الله الرحمن الرحیم
+#in the name of god
+#import "turtle"
 import turtle
 
+# Create a list include color of program
 colors = ["red", "purple", "blue", "green", "yellow", "orange","pink","brown"]
 
+#set background color
 turtle.bgcolor("black")
 
+#create main loop
 for i in range(368):
+    #speed of cursor
     turtle.speed(99999999999999999999999999999999999999999)
+    # use list colors
     turtle.pencolor(colors[i % 6])
     turtle.width(i / 180+1)
     turtle.forward(i)
